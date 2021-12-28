c700b7117e8c9a6a4646e3afebb5ed7caed1954e
Jon Linkens <jonlinkens@outlook.com>
2021-12-28 14:50:00 -0600

    add in recommendation endpoint and logic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from recommender import Recommender
+
+
+rec = Recommender("data/anime_similarity_2020_cleaned.csv.gz")
 
 app = FastAPI()
 
@@ -10,10 +13,28 @@ def read_root():
     return {"Hello": "World"}
 
 
-# endpoint for waking up the API when deployed on heroku
-# a CRON job from https://cron-job.org/en/ pings the api every 15 minutes
-# so it stays up 24/7
-# probably a better way to do this though!
 @app.get("/wake", status_code=200)
 def wake_api():
+    """
+    endpoint for waking up the API when deployed on heroku
+
+    a CRON job from https://cron-job.org/en/ pings the api every 15 minutes
+    so it stays up 24/7
+
+    probably a better way to do this though!
+
+    """
     return {"api alive"}
+
+
+@app.get("/recommend/{anime_name}")
+def recommend(anime_name):
+    """
+    endpoint for returning recommendations
+
+    anime is attached as part of path
+
+    returns an object with an array of the top 5 recommended anime
+    """
+    recommended_anime = rec.recommend(anime_name)
+    return{"anime": recommended_anime}
