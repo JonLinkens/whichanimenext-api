45a7687f1e3525b468e25e2d2120b759b58a5383
Jon Linkens <jonlinkens@outlook.com>
2021-12-28 14:10:31 -0600

    add wake api endpoint

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+# endpoint for waking up the API when deployed on heroku
+# a CRON job from https://cron-job.org/en/ pings the api every 15 minutes
+# so it stays up 24/7
+# probably a better way to do this though!
+@app.get("/wake", status_code=200)
+def wake_api():
+    return {"api alive"}
